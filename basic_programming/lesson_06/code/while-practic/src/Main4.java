0ea715885ce2a765a6d8bdb45f5b340973967432
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-05-26 10:18:03 +0200

    update lesson

--- basic_programming/lesson_06/code/while-practic/src/Main4.java
+++ basic_programming/lesson_06/code/while-practic/src/Main4.java
@@ -1,16 +1,16 @@
-//Вводится число, напечатайте это число в обратном порядке
-
 import java.util.Scanner;
 
 public class Main4 {
     public static void main(String[] args) {
+
+        //Вводится число, напечатайте это число в обратном порядке
         Scanner scanner = new Scanner(System.in); // включаем Scanner
-        System.out.println("Input number (positive and integer)");
-        int number = scanner.nextInt();
+        System.out.println("Input number (positive and integer)"); // invitation for user
+        int number = scanner.nextInt(); // read data
         int numberNew = 0;
-        while (number > 0) {
-            numberNew = numberNew * 10 + number % 10;
-            number = number / 10;
+        while (number > 0) { // loop start
+            numberNew = numberNew * 10 + number % 10; // new number creation
+            number = number / 10; // изменение параметра цикла, change loop parameter/argument
         }
         System.out.println("New number is " + numberNew);
     }
--- basic_programming/lesson_06/code/while-practic/src/Main5.java
+++ basic_programming/lesson_06/code/while-practic/src/Main5.java
@@ -1,29 +1,31 @@
-// Вводится шестизначное число (номер автобусного билета). Определите,
-// является ли этот билет "счастливым"
-// (сумма первых трех цифр равна сумме трех последних цифр).
-
 import java.util.Scanner;
 
 public class Main5 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // включаем Scanner
-        System.out.println("Input number (positive and integer)");
-        int number = scanner.nextInt();
-        int number1= number/1000;
-        number = number % 1000;
 
-        int sum1 = sumTicket(number);
-        int sum2 = sumTicket(number1);
-        System.out.println("Left " + number1);
-        System.out.println("Right " + number);
-        System.out.println("Summa Right is " + sum1);
-        System.out.println("Summa Left is " + sum2);
+        // Вводится шестизначное число (номер автобусного билета). Определите,
+        // является ли этот билет "счастливым"
+        // (сумма первых трех цифр равна сумме трех последних цифр).
+
+        Scanner scanner = new Scanner(System.in); // включаем Scanner
+        System.out.println("Input number (positive and integer) 6 digits");
+        int number = scanner.nextInt(); // read data from keyboard
+        int number1= number/1000; // get 3 first digits, first3Digits - is better
+        number = number % 1000; // rest 3 last digits, last3Digits - is better
+
+        int sum1 = sumTicket(number); // call method
+        int sum2 = sumTicket(number1); // call method
+        System.out.println("Left part of Ticket " + number1);
+        System.out.println("Right part of Ticket " + number);
+        System.out.println("Summa Right is: " + sum1);
+        System.out.println("Summa Left is: " + sum2);
         if (sum1==sum2) {
-            System.out.println("Ticket happy! ");
+            System.out.println("Ticket is happy! ");
         } else {
-            System.out.println("Ticket unhappy( ");
+            System.out.println("Ticket is unhappy ( ");
         }
     }
+    //____________Methods___________
     public static int sumTicket(int num) {
         int sum = 0;
         int count= 0;
@@ -34,4 +36,5 @@ public class Main5 {
         }
         return sum;
     }
+    //____________end of Methods___________
 }
