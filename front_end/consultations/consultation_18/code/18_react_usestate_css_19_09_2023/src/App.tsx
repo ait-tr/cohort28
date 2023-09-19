a05ceb4846b310b663e198609fefc7fbe89e69cd
IGOR_EIDELMAN <v1148656@gmail.com>
2023-09-19 20:26:24 +0300

    Add code of consultation_18

---
+++ front_end/consultations/consultation_18/code/18_react_usestate_css_19_09_2023/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className='btn btn-primary color'>Click me!</button>
+    </div>
+  );
+}
+
+export default App;
