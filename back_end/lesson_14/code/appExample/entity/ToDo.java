4cada66d8fa21cd94fd9032f59adb039db0dca1f
StanislavPB <perminovsb@gmail.com>
2023-11-10 01:56:35 +0000

    add: lesson 13 (lessoncode)

---
+++ back_end/lesson_14/code/appExample/entity/ToDo.java
@@ -0,0 +1,12 @@
+package lesson13.appExample.entity;
+
+import java.time.LocalDateTime;
+
+public class ToDo {
+    private Integer id;
+    private String title;
+    private String description;
+    private LocalDateTime creationDate;
+    private User author;
+
+}
---
+++ back_end/lesson_14/code/appExample/entity/User.java
@@ -0,0 +1,9 @@
+package lesson13.appExample.entity;
+
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+
+}
