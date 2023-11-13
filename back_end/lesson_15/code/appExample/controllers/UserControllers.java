d5c14b8331a5a1da882794b5bf9ea10435f3b9aa
StanislavPB <perminovsb@gmail.com>
2023-11-13 11:56:08 +0000

    add: lesson 15 (lessoncode)

--- back_end/lesson_15/code/appExample/controllers/UserControllers.java
+++ back_end/lesson_15/code/appExample/controllers/UserControllers.java
@@ -1,12 +1,31 @@
 package lesson15.appExample.controllers;
 
+import lesson15.appExample.DTO.UserDtoRequest;
+import lesson15.appExample.DTO.UserDtoResponse;
+import lesson15.appExample.core.services.UserServices;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api")
 public class UserControllers {
+    private final UserServices service;
 
+    @PostMapping("/")
+    public UserDtoResponse addUser(@RequestBody UserDtoRequest userDto) {
+        return service.createUser(userDto);
+    }
+
+    @GetMapping("/users")
+    public List<UserDtoResponse> findAllUsers() {
+        return service.findAllUsers();
+    }
+
+    @GetMapping("/user")
+    public UserDtoResponse findUserByEmail(@RequestParam String email) {
+        return service.findUserByEmail(email);
+    }
 }
