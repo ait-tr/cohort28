388f524730937a0ee9344f1bb2e672d69756dbb1
StanislavPB <perminovsb@gmail.com>
2023-11-13 10:58:36 +0000

    add: lesson 15 (lessoncode)

--- back_end/lesson_15/code/appExample/core/services/TodoServices.java
+++ back_end/lesson_15/code/appExample/core/services/TodoServices.java
@@ -3,12 +3,14 @@ package lesson15.appExample.core.services;
 import lesson15.appExample.DTO.ToDoDtoRequest;
 import lesson15.appExample.DTO.ToDoDtoResponse;
 import lesson15.appExample.core.util.TodoConverters;
+import lesson15.appExample.core.validation.NotFoundException;
 import lesson15.appExample.entity.ToDo;
 import lesson15.appExample.repository.ToDoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -39,5 +41,63 @@ public class TodoServices {
                 .map(converters::converterFromTodoToResponse)
                 .toList();
     }
+
+    //     findTodoById
+
+    public ToDoDtoResponse findById(Integer id) {
+
+        Optional<ToDo> toDoOptional = repository.findById(id);
+
+        if (toDoOptional.isPresent()) {
+            return converters.converterFromTodoToResponse(toDoOptional.get());
+        } else {
+            throw new NotFoundException("ToDo with id " + id + " not found");
+        }
+
+    }
+
+    //findTodoByUserId
+
+    public List<ToDoDtoResponse> findTodoByUserId(Integer userId){
+        return repository.findByUserId(userId)
+                .stream().map(converters::converterFromTodoToResponse)
+                .toList();
+    }
+
+    //updateTodo
+    public ToDoDtoResponse updateToDo(Integer id, ToDoDtoRequest toDoRequest) {
+        Optional<ToDo> toDoOptional = repository.findById(id);
+        if (toDoOptional.isPresent()) {
+            ToDo todoForUpdate = toDoOptional.get();
+            if (toDoRequest.getTitle() != null) {todoForUpdate.setTitle(toDoRequest.getTitle());}
+            if (toDoRequest.getDescription() != null) {todoForUpdate.setDescription(toDoRequest.getDescription());}
+            ToDo updateToDo = repository.save(todoForUpdate);
+            return converters.converterFromTodoToResponse(updateToDo);
+        } else {
+            throw new NotFoundException("ToDo with id " + id + " not found");
+        }
+    }
+
+    /*
+    public ToDoDtoResponse updateToDo (ToDoDtoRequest request){
+        ToDo toDo = converters.converterFromRequestToTodo(request);
+        repository.update(toDo);
+        return converters.converterFromTodoToResponse(repository.findById(toDo.getId()).get());
+    }
+     */
+
+    //deleteTodo
+
+    public boolean delete(Integer id) {
+        Optional<ToDo> toDoOptional = repository.findById(id);
+        if (toDoOptional.isEmpty()) {
+            throw new NotFoundException("ToDo with id " + id + " not found");
+        }
+        repository.delete(id);
+        return true;
+
+
+    }
+
 
 }
--- back_end/lesson_15/code/appExample/core/services/UserServices.java
+++ back_end/lesson_15/code/appExample/core/services/UserServices.java
@@ -1,4 +1,55 @@
-package lesson14.appExample.core.services;
+package lesson15.appExample.core.services;
 
+import lesson15.appExample.DTO.UserDtoRequest;
+import lesson15.appExample.core.validation.IsAlreadyExistException;
+import lesson15.appExample.core.validation.NotFoundException;
+import lesson15.appExample.entity.User;
+import lesson15.appExample.repository.UserRepository;
+import lesson15.appExample.DTO.UserDtoResponse;
+import lesson15.appExample.core.util.UserConverters;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class UserServices {
+    private final UserRepository userRepository;
+    private final UserConverters converters;
+
+
+    // Получить список всех пользователей
+
+    public List<UserDtoResponse> findAllUsers() {
+        return userRepository.findAll().stream()
+                .map(converters::converterFromUserToResponse)
+                .toList();
+    }
+
+    //Найти пользователя по email
+
+    public UserDtoResponse findUserByEmail(String email){
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        if (userOptional.isPresent()) {
+            return converters.converterFromUserToResponse(userOptional.get());
+        } else {
+            throw new NotFoundException("User not found with email: " + email);
+        }
+    }
+
+    // Создать нового пользователя
+
+    public UserDtoResponse createUser(UserDtoRequest request){
+        if (userRepository.findByEmail(request.getEmail()).isEmpty()) {
+            User newUser = converters.converterFromRequestToUser(request);
+
+            User sevedUser = userRepository.save(newUser);
+            return converters.converterFromUserToResponse(sevedUser);
+        } else {
+            throw new IsAlreadyExistException("Manager with email: " + request.getEmail() + " is already exist!");
+        }
+    }
+
 }
